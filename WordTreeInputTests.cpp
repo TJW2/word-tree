006cfbe83ae1ca3414626d54cd19800b464751b9
TJW2 <tom.watson@metaswitch.com>
2016-07-13 10:51:30 +0000

    Add Google Tests for WordTreeInput

---
+++ WordTreeInputTests.cpp
@@ -0,0 +1,11 @@
+/// WordTreeInput Tests using Google Test
+/// TJW2
+
+#include "WordTreeInput.h"
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
