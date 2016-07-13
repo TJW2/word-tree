bebf993ea1078b5f686cfb44bd723e33f0f6cf8e
TJW2 <tom.watson@metaswitch.com>
2016-07-13 11:05:20 +0000

    Add basic WordTreeInput test

--- WordTreeInputTests.cpp
+++ WordTreeInputTests.cpp
@@ -4,6 +4,13 @@
 #include "WordTreeInput.h"
 #include "gtest/gtest.h"
 
+TEST(WordTreeInput, BasicInput)
+{
+  std::string file_path = "test-files/test-1.txt";
+  EXPECT_EQ(most_common_word_from_file(file_path), "C");
+  EXPECT_EQ(count_of_most_common_word_from_file(file_path), 2);
+}
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
